c27ec88311f3d6d5c4b166d7646c277342ea770a
Braian Lopez <braian.lopez.ze@outlook.com>
2021-01-14 01:28:41 -0300

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const route = require('./routes')
 
 //Middlwares
+app.use(cors())
 app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }))
 
